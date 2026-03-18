ddb38c342b16bf7cdd853592dc8fce7843d7ddc1
LazyYeou <lazyyeou@gmail.com>
2026-03-18 12:54:46 +0700

    feat: add vercel analytic

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <Navbar />
       <main>
         <Hero />
